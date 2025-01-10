e203d7e67b9f619ae72a6a766d391506404eaffc
carl-debug <carlelibognalbal09@gmail.com>
2025-01-10 13:38:33 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [step, setStep] = useState(1);
+
+  const countingNumbers = Array.from({ length: 31 }, (_, i) => i + 1);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    setStep((s) => s + 1);
+  }
+
+  const [multi, setMulti] = useState(1);
+
+  function multiNext() {
+    setMulti((m) => (m === 1 ? m * step : m + step));
+  }
+
+  function multiPrevious() {
+    setMulti((m) => (m === 1 ? m * step : m - step));
+  }
+
+  return (
+    <div className="App">
+      <Step
+        step={step}
+        handlePrevious={handlePrevious}
+        handleNext={handleNext}
+        countingNumbers={countingNumbers}
+      />
+      <Count
+        step={step}
+        multi={multi}
+        multiNext={multiNext}
+        multiPrevious={multiPrevious}
+      />
+    </div>
+  );
+}
+
+function Step({ step, handlePrevious, handleNext, countingNumbers }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button onClick={handlePrevious}>-</button>
+      <p>Step: {countingNumbers[step - 1]}</p>
+      <button onClick={handleNext}>+</button>
+    </div>
+  );
+}
+
+function Count({ multi, step, multiNext, multiPrevious }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button onClick={multiPrevious}>-</button>
+      <p>Count:{multi}</p>
+      <button onClick={multiNext}>+</button>
+    </div>
+  );
+}
